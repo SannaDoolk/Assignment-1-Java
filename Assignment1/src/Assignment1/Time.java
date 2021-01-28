cc042a998f8a45a408c511813bf12d517415dc3f
Sanna Doolk <sd222si@student.lnu.se>
2021-01-28 15:22:37 +0100

    Did Time task

---
+++ Assignment1/src/Assignment1/Time.java
@@ -0,0 +1,20 @@
+package Assignment1;
+
+import java.util.Scanner;
+
+public class Time {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Give a number of seconds: ");
+        int secondsInput = scan.nextInt();
+
+        int hours = secondsInput / 60 / 60;
+        int minutes = secondsInput / 60 - (hours * 60);
+        int seconds = secondsInput % 60;
+
+        System.out.print(hours + " hours, " + minutes + " minutes, " + seconds + " seconds.");
+
+        scan.close();
+    }
+}
