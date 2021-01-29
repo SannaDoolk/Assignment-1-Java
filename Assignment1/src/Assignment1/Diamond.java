83f6e9594db25d98b43f1fae27c8cf4f50c70825
Sanna Doolk <sd222si@student.lnu.se>
2021-01-29 12:49:37 +0100

    Added reverse for loop in Diamond and validation for  input

--- Assignment1/src/Assignment1/Diamond.java
+++ Assignment1/src/Assignment1/Diamond.java
@@ -8,18 +8,37 @@ public class Diamond {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter a positive number: ");
         int number = scan.nextInt();
-        String star = "*";
-        int space = number - 1;
 
-        for (int i = 1; i <= number; i++) {
-            for(int j = 1; j < space; j++) {
-                System.out.print("1");
-            }
+        if (number < 0) {
+            System.out.println("That's not a positive number.");
+        } else {
+            String star = "*";
+            int space = number - 1;
+
+            for (int i = 1; i <= number; i++) {
+                for(int j = 1; j <= space; j++) {
+                    System.out.print(" ");
+                }
             space--;
-            for (int j = 1; j <= 2 * i - 1; j++) {
+                for (int j = 1; j <= 2 * i - 1; j++) {
                 System.out.print(star);
-            }
+                }
             System.out.println();
+        }
+
+        space = 1;
+
+            for (int i = 1; i <= number - 1; i++) {
+                for(int j = 1; j <= space; j++) {
+                System.out.print(" ");
+                }
+            space++;
+                for (int j = 1; j <= 2 * (number - i) - 1; j++) {
+                System.out.print(star);
+                }
+            System.out.println();
+        }
+        scan.close();
         }
     }
 }
