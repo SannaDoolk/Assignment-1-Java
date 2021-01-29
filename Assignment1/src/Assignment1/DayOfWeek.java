f67f7d1bdc77b41e629f5727f3c115a612439897
Sanna Doolk <sd222si@student.lnu.se>
2021-01-29 15:13:49 +0100

    Put up the scanner and outprints for DayOfWeek

---
+++ Assignment1/src/Assignment1/DayOfWeek.java
@@ -0,0 +1,21 @@
+package Assignment1;
+
+import java.util.Scanner;
+
+public class DayOfWeek {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a year: ");
+        int year = scan.nextInt();
+        System.out.println("Enter a month (1-12): ");
+        int month = scan.nextInt();
+        System.out.println("Enter day of the month (1-31): ");
+        int day = scan.nextInt();
+
+        
+
+
+        scan.close();
+    }
+}
