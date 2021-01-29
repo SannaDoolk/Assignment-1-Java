a9882263dbc2be7d8411c72a19a208586977045c
Sanna Doolk <sd222si@student.lnu.se>
2021-01-29 12:19:06 +0100

    Added to for loop in Diamond, problems with writing too many stars

--- Assignment1/src/Assignment1/Diamond.java
+++ Assignment1/src/Assignment1/Diamond.java
@@ -8,12 +8,18 @@ public class Diamond {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter a positive number: ");
         int number = scan.nextInt();
+        String star = "*";
+        int space = number - 1;
 
         for (int i = 1; i <= number; i++) {
-            for (int j = 0; j <= number; j++) {
-                System.out.println(" ");
+            for(int j = 1; j < space; j++) {
+                System.out.print("1");
             }
-            
+            space--;
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print(star);
+            }
+            System.out.println();
         }
     }
 }
