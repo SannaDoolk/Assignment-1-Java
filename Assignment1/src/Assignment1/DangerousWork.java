8b8e27dbcefaff45776a99b6674f483d65516df8
Sanna Doolk <sd222si@student.lnu.se>
2021-01-29 08:42:34 +0100

    Started on task DangerousWork

---
+++ Assignment1/src/Assignment1/DangerousWork.java
@@ -0,0 +1,17 @@
+package Assignment1;
+
+import java.util.Scanner;
+
+public class DangerousWork {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How much money do you want to earn?");
+
+        int moneyToEarn = scan.nextInt();
+        int daysToWork;
+
+        for (int i = 0; i < moneyToEarn; i++) {
+            
+        }
+    }
+}
