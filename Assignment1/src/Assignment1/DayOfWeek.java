b7892210d0c8e3c363d74a1a14b2c50b73578628
Sanna Doolk <sd222si@student.lnu.se>
2021-02-01 16:09:34 +0100

    Worked on the math formula for DayOfWeek

--- Assignment1/src/Assignment1/DayOfWeek.java
+++ Assignment1/src/Assignment1/DayOfWeek.java
@@ -6,15 +6,35 @@ public class DayOfWeek {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        int h;
+        int q;
+        int m;
+        int k;
+        int j;
+
         System.out.println("Enter a year: ");
         int year = scan.nextInt();
+
         System.out.println("Enter a month (1-12): ");
-        int month = scan.nextInt();
+        m = scan.nextInt();
+
         System.out.println("Enter day of the month (1-31): ");
-        int day = scan.nextInt();
+        q = scan.nextInt();
 
+        if (m == 1) {
+            m = 13;
+            year -= 1;
+        } else if (m == 2) {
+            m = 14;
+            year -=1;
+        }
+
+        k = (year % 100);
+        j = (year / 100);
+   
+        h = q + (26 * (m + 1) / 10) + k + (k / 4) + (j / 4) + (5 * j);
         
-
+        System.out.println(h % 7);
 
         scan.close();
     }
