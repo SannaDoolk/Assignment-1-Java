31dc8be8884378bf9720e20129dcc2e17d029b62
Sanna Doolk <sd222si@student.lnu.se>
2021-02-04 16:05:32 +0100

    Worked on getting ant to walk in direction and not jump randomly in Ant

--- Assignment1/src/Assignment1/Ants.java
+++ Assignment1/src/Assignment1/Ants.java
@@ -4,7 +4,17 @@ import java.util.Random;
 
 public class Ants {
     public static void main(String[] args) {
-        
+        int result = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            result += numOfSteps();
+        }
+        int average = (result / 10);
+        System.out.println("Average steps is: " + average);   
+    }
+
+    public static int numOfSteps() {
+
         Random rnd = new Random();
         int [][] array2D = new int[8][8];
         array2D[0][0] = 0;
@@ -12,24 +22,49 @@ public class Ants {
         int visitedSquares = 0;
         int steps = 0;
 
+        int x = rnd.nextInt(8);
+        int y = rnd.nextInt(8);
+
         while (visitedSquares < 64) {
-            int x = rnd.nextInt(8);
-            int y = rnd.nextInt(8);
-            
+
             if (array2D[x][y] == 0) {
-                array2D[x][y] = 1;
+                array2D[x][y] += 1;
                 visitedSquares++;
+            }   
+
+            int rndDir = randomDirection();
+
+            if (rndDir == 0) {
+                y--;
+                if (y < 0) {
+                y++;
+                }
+            } else if (rndDir == 1) {
+                y++;
+                if (y > 7) {
+                    y--; 
             }
+            } else if (rndDir == 2) {
+                    x--;
+                if (x < 0) {
+                    x++;
+                } 
+            } else if (rndDir == 3) {
+                   x++;
+                if (x > 7) {
+                   x--;
+                } 
+            }
+
             steps++;
         }
-
-        for (int i = 0; i < array2D.length; i++) {
-            for (int j = 0; j < array2D.length; j++) {
-                System.out.print(array2D[i][j] + " ");
-            }
-            System.out.println();
+        System.out.println("Number of steps in simulation: " + steps);
+        return steps;
         }
-        System.out.println(steps);
 
-    }
+        public static int randomDirection() {
+            Random rnd = new Random();
+            int z = rnd.nextInt(4);
+            return z;
+        }
 }
